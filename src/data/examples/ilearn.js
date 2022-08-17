dc6279b5ba33afd5415f46dbb928e595b1a7a56b
Ben De Meester <ben.demeester@ugent.be>
2022-08-17 10:58:45 +0200

    added deployment

---
+++ src/data/examples/ilearn.js
@@ -0,0 +1,7 @@
+const data = require('./ilearn.json');
+
+export const title = "i-Learn onderwijsdoelen"
+
+export const globalDefaultsJSON = data.globalDefaults;
+export const nodesJSON = data.nodes;
+export const edgesJSON = data.edges;
